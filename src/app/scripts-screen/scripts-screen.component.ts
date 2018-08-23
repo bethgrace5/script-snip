a4f4ac90e58e24b8a2c94e2dfd9903be88d14af4
Bethany Armitage <bethgrace5@gmail.com>
2018-08-22 19:09:40 -0700

    Internals: Add additional scripts to scripts screen

--- src/app/scripts-screen/scripts-screen.component.ts
+++ src/app/scripts-screen/scripts-screen.component.ts
@@ -9,18 +9,38 @@ import { Script } from '../models/script';
 export class ScriptsScreenComponent implements OnInit {
 
   public script1: Script
+  public script2: Script
+  public script3: Script
 
   constructor() { }
 
   ngOnInit() {
+
     this.script1 = {
-      command: "script 1 command",
+      command: "script 1 command here",
       flags: [
         "-f1",
-        "-f2",
+        "-f5",
+        "-f4",
+        "-f3",
         "--other-flag"
       ]
     }
+    this.script2 = {
+      command: "two",
+      flags: [
+        "-f3",
+        "--other-flag"
+      ]
+    }
+    this.script3 = {
+      command: "three",
+      flags: [
+        "-f3",
+        "--other-flag"
+      ]
+    }
+
   }
 
 }
