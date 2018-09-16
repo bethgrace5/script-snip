33ce6b1ab72d87bd95a7c7df5d520ff701d28fe0
charmgeorge <charmcgeorge@yahoo.com>
2018-09-15 19:28:27 -0700

    added scriptAndFlag prop, removed clipboard function and remove comma separation

--- src/app/script-card/script-card.component.ts
+++ src/app/script-card/script-card.component.ts
@@ -9,21 +9,14 @@ import { Script } from '../models/script';
 export class ScriptCardComponent implements OnInit {
   @Input() script: Script;
 
-  textToCopy: string
+  textToCopy: string;
+  scriptAndFlag: string;
 
   constructor() { }
 
   ngOnInit() {
     console.log("init")
-    this.scriptAndFlag = this.script.command + this.script.flags
-  }
-
-  public copyToClipboard() {
-    console.log('I want this script in my clipboard: \"', this.script.command, '\"');
-    console.log('but, it needs all of the flags also, something like: [script flag flag flag]');
-
-    let stringInterpolation: string = 'String Interpolation';
-    console.log(`I suggest using ${stringInterpolation}`);
+    this.scriptAndFlag = this.script.command + " " + this.script.flags.join(" ")
   }
 
 }
